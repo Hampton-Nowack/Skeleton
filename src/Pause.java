c62aaf47307cca1ca8a15948ef704ab46cc2dc33
Hampton-Nowack <47673446+Hampton-Nowack@users.noreply.github.com>
2019-11-11 15:18:39 -0500

    Finished?
    implemented the rest of the sounds we needed and allowed the pause scene to return the menu scene.

--- src/Pause.java
+++ src/Pause.java
@@ -6,28 +6,36 @@ import edu.utc.game.*;
 public class Pause extends Game implements Scene {
 	private Scene returnTo;
 	private SkeletonGame.Score score; 
+	SimpleMenu menu; 
 	public List<Text> boxes; 
 	Target t1;
 	Target t2; 
 	
+	//pause constructor takes in the scene that returned pause menu and the current score
 	public Pause(Scene returnTo, SkeletonGame.Score score) {
 		this.returnTo = returnTo;
 		this.score = score; 
 		boxes = new java.util.LinkedList<>(); 
+		
+		//create new selectable text boxes
 		t1 = new Target(100, 100, "Continue");
-		t2 = new Target(100, 150, "Exit"); 
+		t2 = new Target(100, 150, "Return to Main Menu"); 
 		
 	}
+	//target class creates selectable text boxes
 	public class Target extends GameObject {
 		public int width;
 		public int height; 
 		
+		//constructs targets
 		public Target (int x, int y, String text) {
 			Text textBox = new Text(x, y, 50, 50, text); 
 			boxes.add(textBox); 
 			hitbox.setSize(text.length() * 50, 50);
 			hitbox.setLocation(x, y); 
 		}
+		
+		//determines if the player is clicking on the textbox
 		public boolean isClicked() {
 			if(Game.ui.mouseButtonIsPressed(0) == true) {
 				XYPair<Integer> mouse = Game.ui.getMouseLocation(); 
@@ -41,19 +49,34 @@ public class Pause extends Game implements Scene {
 		
 	}
 	
-
+	//draws and updates objects
 	public Scene drawFrame(int delta) {
+		
+		//if the player tries to continue game, setScene to true so clicking can update score and continue the timer
 		if (t1.isClicked()) {
 			score.setScene(true);
 			score.startTime();
 			return returnTo; 
 		}
-		if (t2.isClicked()) {
-			return null; 
+		
+		//if the player wants to return to the main menu, setScene to true, reset score to zero, and create a new main menu scene
+		//and return to it
+		if (t2.isClicked()) 
+		{
+			score.setScene(true);
+			score.reset(); 
+			menu = new SimpleMenu(); 
+			menu.addItem(new SimpleMenu.SelectableText(20, 20, 20, 20, "Launch Game", 1, 0, 0, 1, 1, 1), returnTo);
+			menu.addItem(new SimpleMenu.SelectableText(20, 60, 20, 20, "Exit", 1, 0, 0, 1, 1, 1), null);
+			menu.select(0);
+			return menu; 
 		}
+		//draw the targets
 		for (Text b : boxes) {
 			b.draw(); 
 		}
+		
+		//draw and update the score
 		score.draw(); 
 		score.update(delta);
 		
--- src/SkeletonGame.java
+++ src/SkeletonGame.java
@@ -2,6 +2,10 @@ import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glClearColor;
+import org.lwjgl.opengl.GL;
+import static org.lwjgl.glfw.GLFW.glfwGetKey;
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
 
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWMouseButtonCallback;
@@ -15,6 +19,8 @@ public class SkeletonGame extends Game implements Scene {
 	public static Scene pause;
 	public static Scene menu; 
 	public static Victory vict; 
+	static Sound click;
+	Sound pauseSound; 
 	
 	
 	public static void main(String[] args) {
@@ -34,72 +40,95 @@ public class SkeletonGame extends Game implements Scene {
 		game.gameLoop();
 	}
 	
+	//Skeleton Game Constructor
 	public SkeletonGame() {
 		initUI(640, 480, "Skeleton");
 		glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+		
+		//initialize the score to 0 every time a new SkeletonGame is created
 		score = 0; 
+		
+		//initialize the sounds for clicking and pausing
+		pauseSound = new Sound("res/pauseSound.wav");
+		click = new Sound("res/click.wav"); 
 		sc = new Score(); 
 
 	}
-	
+	//The score class keeps track of the total score, time, and whether or not the current scene can add score or not
 	public static class Score extends GameObject {
+		public boolean pressed = false; 
 		public long startTime;
 		public long endTime;
 		public long sumTime; 
 		public Text scoreText = new Text(20, 30, 20,20, "0");
 		public boolean scene = true; 
 		int counter = 0; 
+		
+		//score constructor
 		public Score() {
 			this.hitbox.setSize(20, 30);
 			this.hitbox.setLocation(10, 4);
 			this.setColor(0,0,0); 
 		}
+		//starts the timer
 		public void startTime() {
 			startTime = System.currentTimeMillis(); 
 		}
+		//stops the timer
 		public void stopTime() {
 			endTime = System.currentTimeMillis();
 			sumTime = sumTime + endTime - startTime; 
 		}
-		
+		//returns the total time
 		public long getTime() {
 			return sumTime; 
 		}
+		//Determines if the mouse button has been released before it can be pressed again
+		public boolean mouseButtonIsPressed(int button) {
+			if(GLFW.glfwGetMouseButton(Game.ui.getWindow(), button) == GLFW_RELEASE) {
+				pressed = true; 
+			}
+			if(GLFW.glfwGetMouseButton(Game.ui.getWindow(), button) == GLFW_PRESS && pressed) {
+				pressed = false;
+				return true; 
+			}
+			else {
+				return false;
+			}
+		}
 		
+		//resets the timer and the score
 		public void reset() {
 			sumTime = 0;
 			score = 0;
 			scoreText = new Text(20, 30, 20,20, String.valueOf(score));
 		}
-	
+		//Determines if the current scene is able to add to score 
 		public void setScene(Boolean scene) {
 			this.scene = scene; 
 		} 
+		//determines if the score should be updated and updates the score and scoreText
 		public void update(int delta) {
-			if(Game.ui.mouseButtonIsPressed(0) == true && counter == 0 && scene) { 
+			if(mouseButtonIsPressed(0) == true && scene) { 
 				if (score == 0) {
 					startTime(); 
 				}
+				click.play();
 				score++; 
 				scoreText = new Text(20, 30, 20,20, String.valueOf(score));
-			}
-			
-			if (counter <= 5) {
-				counter++;
-			}
-			else {
-				counter= 0; 
 			}
 		}
 	}
 	
-	
+	//draws and updates all objects
 	public Scene drawFrame(int delta) {
 		
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
 
 		sc.scoreText.draw(); 
 		sc.update(delta);
+		
+		//creates and returns new victory screen if the score is 10
 		if(score == 10) {
 			sc.stopTime(); 
 			double timeOut = (double)(sc.getTime() * .001);
@@ -107,7 +136,9 @@ public class SkeletonGame extends Game implements Scene {
 			vict.setScore(sc, timeOut);
 			return vict; 
 		}
+		//returns the pause scene and stops the timer
 		if (Game.ui.keyPressed(org.lwjgl.glfw.GLFW.GLFW_KEY_P)) {
+			pauseSound.play(); 
 			Pause pause = new Pause(this, sc); 
 			sc.setScene(false); 
 			sc.stopTime(); 
